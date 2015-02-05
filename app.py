2b3702ec2a6818d62b46a28a53b87de1e87b2953
David Awad <david.awad@addteq.com>
2015-02-05 17:20:21 +0000

    cloning Repo's

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import sys
+import git
 
 
 
+print "paste repo URL here"
+url = raw_input()
+
+print url
+
+git.Repo.clone_from('https://github.com/DavidAwad/Arduino', 'local/')
 
 
 
-print 'Success' 
+print 'Success'
